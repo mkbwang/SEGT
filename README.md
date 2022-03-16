62cd531f8671a97dced6c2835f4a8c0d3c98bba5
Mukai Wang <wangmk@umich.edu>
2022-03-16 16:36:47 -0400

    First sketch of the static webpage.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # SEGT
 Spatial Evolutionary Game Theory
 
+[D3js Official site](https://d3js.org/)
+
+[D3js official Wiki](https://github.com/d3/d3/wiki)
+
 [P5js official site](https://p5js.org/)
 
 [P5js Github Wiki](https://github.com/processing/p5.js/wiki)
--- sketch.js
+++ sketch.js
@@ -1,26 +1,61 @@
-const s = (sketch) =>{
-  sketch.setup = () => {
-    sketch.createCanvas(400, 400);
-    sketch.noStroke();
-    sketch.fill(40,245,200);
-  };
-  sketch.draw = () => {
-    sketch.background(200,150,245);
-
-  // distance between squares
-    let jump = 40;
-    
-    // two for loops are needed to make a grid of squares
-    // the outer loop controls how many rows are made
-    // the inner loop controls how many columns are made
-    for (let y = 0; y < 10; y++) {
-      for (let x = 0; x < 10; x++) {
-        
-        sketch.rect(x * jump + 10, y * jump + 10, 20, 20);
-        
-      }
-    }
-  }
+function gridData() {
+	let data = new Array();
+	let xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
+	let ypos = 1;
+	let width = 20;
+	let height = 20;
+	let click = 0;
+	
+	// iterate for rows	
+	for (let row = 0; row < 20; row++) {
+		data.push( new Array() );
+		
+		// iterate for cells/columns inside rows
+		for (var column = 0; column < 20; column++) {
+			data[row].push({
+				x: xpos,
+				y: ypos,
+				width: width,
+				height: height,
+				click: click
+			})
+			// increment the x position. I.e. move it over by 50 (width variable)
+			xpos += width;
+		}
+		// reset the x position after a row is complete
+		xpos = 1;
+		// increment the y position for the next row. Move it down 50 (height variable)
+		ypos += height;	
+	}
+	return data;
 }
 
-let myp5 = new p5(s, 'p5sketch');
+let mygrid = gridData();	
+// I like to log the data to the console for quick debugging
+// console.log(mygrid);
+
+let grid = d3.select("#d3sketch")
+	.append("svg")
+	.attr("width","510px")
+	.attr("height","510px");
+	
+let row = grid.selectAll(".row")
+	.data(mygrid)
+	.enter().append("g")
+	.attr("class", "row");
+	
+let column = row.selectAll(".square")
+	.data(function(d) { return d; })
+	.enter().append("rect")
+	.attr("class","square")
+	.attr("x", function(d) { return d.x; })
+	.attr("y", function(d) { return d.y; })
+	.attr("width", function(d) { return d.width; })
+	.attr("height", function(d) { return d.height; })
+	.style("fill", "#F56C4E")
+	.style("stroke", "#222")
+	.on('click', function(d) {
+       d.click ++;
+       if ((d.click)%2 == 0 ) { d3.select(this).style("fill","#F56C4E"); }
+	   if ((d.click)%2 == 1 ) { d3.select(this).style("fill","#2C93E8"); }
+    });
